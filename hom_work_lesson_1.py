f24793ad2b3b691436038e8c5633badbe7e9b97c
Makgaffin <makgaffin@gmail.com>
2024-07-09 08:56:14 +0300

    Add files via upload

---
+++ hom_work_lesson_1.py
@@ -0,0 +1,26 @@
+
+    #  2023/09/21 00:00|Практическое задание по уроку "Строки и индексация строк"
+
+
+ # 1
+
+example = "Dima"
+print(example[0])
+
+ # 2
+
+example = "Dima"
+print(example[-1])
+
+ # 3
+
+example = "Dima"
+print(example[2:])
+
+ # 4
+
+example = "Dima"
+print(example[::-1])
+
+example = "Топинамбур"
+print(example[1:10:2])
